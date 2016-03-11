9352a67949f1968c7a663d06d76800ca9ee9b9c2
DanNeciu <DanNeciu>
2016-03-11 11:15:05 +0200

    fixed Infinit scroll

--- src/components/topic.jsx
+++ src/components/topic.jsx
@@ -1,47 +1,64 @@
 var React = require('react');
 var Actions = require('../actions');
 var ImageStore = require('../stores/image-store');
+var NextImages = require('../stores/image-store-2');
 var Reflux = require('reflux');
 var InfiniteScroll = require('react-infinite-scroll')(React);
 var ImagePreview =require('./image-preview');
 var ReactDOM = require('react-dom');
 var topic = React.createClass({
 	mixins:[
-		Reflux.listenTo(ImageStore,'onChange')
+	Reflux.listenTo(ImageStore,'onChange'),
+	Reflux.listenTo(NextImages,'onChange2')
 	],
 	getInitialState:function(){
 		return{
 			images:[],
 			page:0,
+			hasMore:true
 		}
 	},
 	componentWillMount:function(){
 		Actions.getImages(this.props.params.id);
+
 	},
 	componentWillReceiveProps:function(nextProps) {
-	      Actions.getImages(nextProps.params.id);
+		Actions.getImages(nextProps.params.id);
 	},
 	render: function() {
 		return <div className="container">
-			<div className="col-md-8 col-sm-6 col-xs-12">
-				<div className="topic ">
-				<InfiniteScroll
-				    pageStart={this.state.page}
-				    loadMore={this.handleLoad}
-				    hasMore={true || false}
-				    loader={<div className="loader">Loading ...</div>}>
-				 	{this.renderImages()}
-				</InfiniteScroll>
-				</div>
-			</div>
-			<div className="col-md-4 col-sm-6 col-xs-12">
-				<div className="back_to_top" onClick={this.HandleClick}></div>
-			</div>
+		<div className="col-md-8 col-sm-6 col-xs-12">
+		<div className="topic ">
+		<InfiniteScroll
+		pageStart={this.state.page}
+		loadMore={this.handleLoad}
+		hasMore={this.state.hasMore}
+		loader={<div className="loader">Loading ...</div>}>
+		{this.renderImages()}
+		</InfiniteScroll>
+		</div>
+		</div>
+		<div className="col-md-4 col-sm-6 col-xs-12">
+		<div className="back_to_top" onClick={this.HandleClick}></div>
+		</div>
 		</div>
 	},
 	handleLoad:function(){
-		console.log(this.state.page);
+		if (this.state.page != 0) {
+			var value = this.props.params.id +"/"+this.state.page;
+			Actions.getImages2(value);
+		}
+		this.setState({
+			page:this.state.page+1,
+			hasMore:false
+		});
 		
+		setTimeout(function () {
+			this.setState({
+				hasMore: true
+			});
+		}.bind(this), 1000);
+		console.log(this.state.page);
 	},
 	HandleClick:function(){
 		ReactDOM.findDOMNode(this).scrollIntoView();
@@ -53,8 +70,12 @@ var topic = React.createClass({
 	},
 	onChange:function(event,images){
 		this.setState({images:images})
+	},
+	onChange2:function(event,images){
+		var old= this.state.images;
+		var children = old.concat(images);
+		this.setState({images:children})
 	}
-
 });
 
 module.exports = topic;
